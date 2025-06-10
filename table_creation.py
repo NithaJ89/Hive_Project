e683e100dcbf48b74e43b997bb5e93fa633004ef
NithaJ89 <nitha.p.joseph@gmail.com>
2025-06-10 15:58:43 +0400

    Create table_creation.py

---
+++ table_creation.py
@@ -0,0 +1,17 @@
+from connection import hiveConnection
+def create_table(tablename, columninfo):
+    try:
+        c = hiveConnection()
+        t = f"""
+        create table if not exists {tablename} (
+                {columninfo}
+        )
+        row format delimited 
+        fields terminated by ','
+        tblproperties ('skip.header.line.count'='1')
+        """
+        c.execute(t)
+        print(f'{tablename} created Successfully...')
+
+    except Exception as e:
+        print(f'Error in table creation of: {tablename} ',e)
